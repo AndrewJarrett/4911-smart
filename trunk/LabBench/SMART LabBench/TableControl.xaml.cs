63cd44ae7e54f8d4e0b136aa4155b7077f6d599b
ajpl.delacroix <ajpl.delacroix@030f3106-02ce-11df-8a57-81850bede9db>
2010-03-27 19:21:37 +0000

    Added DataStructures (Node, NodeList, Graph) and a template for Dijkstra's algorithm. Also includes all the usual SMART Table code.

---
+++ trunk/LabBench/SMART LabBench/TableControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using libSMARTMultiTouch.Controls;
+
+namespace SMART_LabBench
+{
+    /// <summary>
+    /// Interaction logic for TableControl.xaml
+    /// </summary>
+    public partial class TableControl : TableApplicationControl
+    {
+        private Canvas canvas = new Canvas();
+
+        public TableControl()
+        {
+            canvas.Background = new SolidColorBrush(Colors.Transparent);
+            InitializeComponent();
+        }
+
+        private void TableApplicationControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            TableLayoutRoot.Children.Add(canvas);
+
+            DraggableImage mDraggableImage = new DraggableImage(20,20,0,new BitmapImage(new Uri("C:\\Users\\Jean-Pierre\\Downloads\\lemon.bmp")),true,1,2);
+            mDraggableImage.Height = 200; mDraggableImage.Width = 200;
+            canvas.Children.Add(mDraggableImage);
+        }
+    }
+}
