07548a22276a06c5cf771a78af3b97466e29a297
ajpl.delacroix <ajpl.delacroix@030f3106-02ce-11df-8a57-81850bede9db>
2010-03-27 19:21:37 +0000

    Added DataStructures (Node, NodeList, Graph) and a template for Dijkstra's algorithm. Also includes all the usual SMART Table code.

---
+++ LabBench/SMART LabBench/DataStructures/ID.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LabBench.DataStructures
+{
+    class ID
+    {
+        private int id;
+
+        public ID(int id)
+        {
+            this.id = id;
+        }
+
+        public int Value
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+    }
+}
