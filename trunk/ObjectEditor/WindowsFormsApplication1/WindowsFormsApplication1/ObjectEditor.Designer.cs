2ac8f4db92eb01ad0fb9479f400bf4b1854508b0
efoley <efoley@030f3106-02ce-11df-8a57-81850bede9db>
2010-03-28 01:22:56 +0000

    Finished object editor to use: -open run.exe -select object directory as: "LabBench/SMART LabBench/objects"

--- trunk/ObjectEditor/WindowsFormsApplication1/WindowsFormsApplication1/ObjectEditor.Designer.cs
+++ trunk/ObjectEditor/WindowsFormsApplication1/WindowsFormsApplication1/ObjectEditor.Designer.cs
@@ -31,17 +31,21 @@
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.FileChoser = new System.Windows.Forms.Button();
             this.objectBrowser = new System.Windows.Forms.TreeView();
-            this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
-            this.imageChooser = new System.Windows.Forms.Button();
             this.objectModifier = new System.Windows.Forms.Panel();
-            this.txtName = new System.Windows.Forms.TextBox();
-            this.lblName = new System.Windows.Forms.Label();
-            this.chkConductive = new System.Windows.Forms.CheckBox();
-            this.chkFerrous = new System.Windows.Forms.CheckBox();
-            this.chkMagnetic = new System.Windows.Forms.CheckBox();
-            this.sldVoltage = new System.Windows.Forms.TrackBar();
-            this.lblVoltage = new System.Windows.Forms.Label();
             this.txtVoltage = new System.Windows.Forms.TextBox();
+            this.lblVoltage = new System.Windows.Forms.Label();
+            this.sldVoltage = new System.Windows.Forms.TrackBar();
+            this.chkMagnetic = new System.Windows.Forms.CheckBox();
+            this.chkFerrous = new System.Windows.Forms.CheckBox();
+            this.chkConductive = new System.Windows.Forms.CheckBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.imageChooser = new System.Windows.Forms.Button();
+            this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.pictureSelector = new System.Windows.Forms.OpenFileDialog();
+            this.createNewCategory = new System.Windows.Forms.Button();
+            this.createNewObject = new System.Windows.Forms.Button();
+            this.saveObject = new System.Windows.Forms.Button();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
@@ -57,14 +61,16 @@
             // 
             // splitContainer1.Panel1
             // 
+            this.splitContainer1.Panel1.Controls.Add(this.createNewObject);
+            this.splitContainer1.Panel1.Controls.Add(this.createNewCategory);
             this.splitContainer1.Panel1.Controls.Add(this.FileChoser);
             this.splitContainer1.Panel1.Controls.Add(this.objectBrowser);
             // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.objectModifier);
-            this.splitContainer1.Size = new System.Drawing.Size(426, 361);
-            this.splitContainer1.SplitterDistance = 204;
+            this.splitContainer1.Size = new System.Drawing.Size(424, 395);
+            this.splitContainer1.SplitterDistance = 203;
             this.splitContainer1.TabIndex = 0;
             // 
             // FileChoser
@@ -85,17 +91,9 @@
             this.objectBrowser.TabIndex = 0;
             this.objectBrowser.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.objectBrowser_AfterSelect);
             // 
-            // imageChooser
-            // 
-            this.imageChooser.Location = new System.Drawing.Point(15, 6);
-            this.imageChooser.Name = "imageChooser";
-            this.imageChooser.Size = new System.Drawing.Size(188, 139);
-            this.imageChooser.TabIndex = 0;
-            this.imageChooser.Text = "Choose Image";
-            this.imageChooser.UseVisualStyleBackColor = true;
-            // 
             // objectModifier
             // 
+            this.objectModifier.Controls.Add(this.saveObject);
             this.objectModifier.Controls.Add(this.txtVoltage);
             this.objectModifier.Controls.Add(this.lblVoltage);
             this.objectModifier.Controls.Add(this.sldVoltage);
@@ -107,55 +105,26 @@
             this.objectModifier.Controls.Add(this.imageChooser);
             this.objectModifier.Location = new System.Drawing.Point(3, 6);
             this.objectModifier.Name = "objectModifier";
-            this.objectModifier.Size = new System.Drawing.Size(215, 355);
+            this.objectModifier.Size = new System.Drawing.Size(215, 389);
             this.objectModifier.TabIndex = 1;
             this.objectModifier.Visible = false;
             // 
-            // txtName
+            // txtVoltage
             // 
-            this.txtName.Location = new System.Drawing.Point(66, 164);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(137, 20);
-            this.txtName.TabIndex = 1;
+            this.txtVoltage.Location = new System.Drawing.Point(17, 293);
+            this.txtVoltage.Name = "txtVoltage";
+            this.txtVoltage.Size = new System.Drawing.Size(43, 20);
+            this.txtVoltage.TabIndex = 8;
+            this.txtVoltage.TextChanged += new System.EventHandler(this.txtVoltage_TextChanged);
             // 
-            // lblName
+            // lblVoltage
             // 
-            this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(22, 167);
-            this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(38, 13);
-            this.lblName.TabIndex = 2;
-            this.lblName.Text = "Name:";
-            // 
-            // chkConductive
-            // 
-            this.chkConductive.AutoSize = true;
-            this.chkConductive.Location = new System.Drawing.Point(25, 190);
-            this.chkConductive.Name = "chkConductive";
-            this.chkConductive.Size = new System.Drawing.Size(80, 17);
-            this.chkConductive.TabIndex = 3;
-            this.chkConductive.Text = "Conductive";
-            this.chkConductive.UseVisualStyleBackColor = true;
-            // 
-            // chkFerrous
-            // 
-            this.chkFerrous.AutoSize = true;
-            this.chkFerrous.Location = new System.Drawing.Point(25, 213);
-            this.chkFerrous.Name = "chkFerrous";
-            this.chkFerrous.Size = new System.Drawing.Size(61, 17);
-            this.chkFerrous.TabIndex = 4;
-            this.chkFerrous.Text = "Ferrous";
-            this.chkFerrous.UseVisualStyleBackColor = true;
-            // 
-            // chkMagnetic
-            // 
-            this.chkMagnetic.AutoSize = true;
-            this.chkMagnetic.Location = new System.Drawing.Point(25, 236);
-            this.chkMagnetic.Name = "chkMagnetic";
-            this.chkMagnetic.Size = new System.Drawing.Size(70, 17);
-            this.chkMagnetic.TabIndex = 5;
-            this.chkMagnetic.Text = "Magnetic";
-            this.chkMagnetic.UseVisualStyleBackColor = true;
+            this.lblVoltage.AutoSize = true;
+            this.lblVoltage.Location = new System.Drawing.Point(14, 277);
+            this.lblVoltage.Name = "lblVoltage";
+            this.lblVoltage.Size = new System.Drawing.Size(46, 13);
+            this.lblVoltage.TabIndex = 7;
+            this.lblVoltage.Text = "Voltage:";
             // 
             // sldVoltage
             // 
@@ -167,28 +136,104 @@
             this.sldVoltage.TickFrequency = 20;
             this.sldVoltage.Scroll += new System.EventHandler(this.sldVoltage_Scroll);
             // 
-            // lblVoltage
+            // chkMagnetic
             // 
-            this.lblVoltage.AutoSize = true;
-            this.lblVoltage.Location = new System.Drawing.Point(14, 277);
-            this.lblVoltage.Name = "lblVoltage";
-            this.lblVoltage.Size = new System.Drawing.Size(46, 13);
-            this.lblVoltage.TabIndex = 7;
-            this.lblVoltage.Text = "Voltage:";
+            this.chkMagnetic.AutoSize = true;
+            this.chkMagnetic.Location = new System.Drawing.Point(25, 236);
+            this.chkMagnetic.Name = "chkMagnetic";
+            this.chkMagnetic.Size = new System.Drawing.Size(70, 17);
+            this.chkMagnetic.TabIndex = 5;
+            this.chkMagnetic.Text = "Magnetic";
+            this.chkMagnetic.UseVisualStyleBackColor = true;
             // 
-            // txtVoltage
+            // chkFerrous
             // 
-            this.txtVoltage.Location = new System.Drawing.Point(17, 293);
-            this.txtVoltage.Name = "txtVoltage";
-            this.txtVoltage.Size = new System.Drawing.Size(43, 20);
-            this.txtVoltage.TabIndex = 8;
-            this.txtVoltage.TextChanged += new System.EventHandler(this.txtVoltage_TextChanged);
+            this.chkFerrous.AutoSize = true;
+            this.chkFerrous.Location = new System.Drawing.Point(25, 213);
+            this.chkFerrous.Name = "chkFerrous";
+            this.chkFerrous.Size = new System.Drawing.Size(61, 17);
+            this.chkFerrous.TabIndex = 4;
+            this.chkFerrous.Text = "Ferrous";
+            this.chkFerrous.UseVisualStyleBackColor = true;
+            // 
+            // chkConductive
+            // 
+            this.chkConductive.AutoSize = true;
+            this.chkConductive.Location = new System.Drawing.Point(25, 190);
+            this.chkConductive.Name = "chkConductive";
+            this.chkConductive.Size = new System.Drawing.Size(80, 17);
+            this.chkConductive.TabIndex = 3;
+            this.chkConductive.Text = "Conductive";
+            this.chkConductive.UseVisualStyleBackColor = true;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(22, 167);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(66, 164);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(137, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // imageChooser
+            // 
+            this.imageChooser.Location = new System.Drawing.Point(15, 6);
+            this.imageChooser.Name = "imageChooser";
+            this.imageChooser.Size = new System.Drawing.Size(188, 139);
+            this.imageChooser.TabIndex = 0;
+            this.imageChooser.Text = "Choose Image";
+            this.imageChooser.UseVisualStyleBackColor = true;
+            this.imageChooser.Click += new System.EventHandler(this.imageChooser_Click);
+            // 
+            // pictureSelector
+            // 
+            this.pictureSelector.Filter = "Bitmap images|*.bmp";
+            this.pictureSelector.FileOk += new System.ComponentModel.CancelEventHandler(this.pictureSelector_FileOk);
+            // 
+            // createNewCategory
+            // 
+            this.createNewCategory.Location = new System.Drawing.Point(0, 360);
+            this.createNewCategory.Name = "createNewCategory";
+            this.createNewCategory.Size = new System.Drawing.Size(103, 35);
+            this.createNewCategory.TabIndex = 2;
+            this.createNewCategory.Text = "New Category";
+            this.createNewCategory.UseVisualStyleBackColor = true;
+            this.createNewCategory.Visible = false;
+            this.createNewCategory.Click += new System.EventHandler(this.createNewCategory_Click);
+            // 
+            // createNewObject
+            // 
+            this.createNewObject.Location = new System.Drawing.Point(103, 360);
+            this.createNewObject.Name = "createNewObject";
+            this.createNewObject.Size = new System.Drawing.Size(98, 35);
+            this.createNewObject.TabIndex = 3;
+            this.createNewObject.Text = "New Object";
+            this.createNewObject.UseVisualStyleBackColor = true;
+            this.createNewObject.Visible = false;
+            this.createNewObject.Click += new System.EventHandler(this.createNewObject_Click);
+            // 
+            // saveObject
+            // 
+            this.saveObject.Location = new System.Drawing.Point(15, 328);
+            this.saveObject.Name = "saveObject";
+            this.saveObject.Size = new System.Drawing.Size(187, 49);
+            this.saveObject.TabIndex = 9;
+            this.saveObject.Text = "Save Object";
+            this.saveObject.UseVisualStyleBackColor = true;
+            this.saveObject.Click += new System.EventHandler(this.saveObject_Click);
             // 
             // mainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(426, 361);
+            this.ClientSize = new System.Drawing.Size(424, 395);
             this.Controls.Add(this.splitContainer1);
             this.Name = "mainWindow";
             this.Text = "Object Creator/Editor";
@@ -218,6 +263,10 @@
         private System.Windows.Forms.TrackBar sldVoltage;
         private System.Windows.Forms.TextBox txtVoltage;
         private System.Windows.Forms.Label lblVoltage;
+        private System.Windows.Forms.OpenFileDialog pictureSelector;
+        private System.Windows.Forms.Button createNewObject;
+        private System.Windows.Forms.Button createNewCategory;
+        private System.Windows.Forms.Button saveObject;
     }
 }
 
--- trunk/ObjectEditor/WindowsFormsApplication1/WindowsFormsApplication1/ObjectEditor.cs
+++ trunk/ObjectEditor/WindowsFormsApplication1/WindowsFormsApplication1/ObjectEditor.cs
@@ -6,11 +6,17 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
 
 namespace WindowsFormsApplication1
 {
     public partial class mainWindow : Form
     {
+        string objectDirectory, currentCategory, currentObject;
+
         public mainWindow()
         {
             InitializeComponent();
@@ -20,29 +26,46 @@ namespace WindowsFormsApplication1
         {
             if (folderBrowser.ShowDialog() == DialogResult.OK)
             {
-                // Initial Node "Categories"
-                TreeNode categoryList = new TreeNode("Categories");
-                this.objectBrowser.Nodes.Add(categoryList);
+                // expose new category and object buttons
+                createNewCategory.Visible = true;
+                createNewObject.Visible = true;
 
-                foreach (string catDirectory in System.IO.Directory.GetDirectories(folderBrowser.SelectedPath))
-                {
-                    // Category Nodes
-                    TreeNode newCategory = new TreeNode(catDirectory.Split('\\').Last());
-                    categoryList.Nodes.Add(newCategory);
+                objectDirectory = folderBrowser.SelectedPath;
 
-                    // Object Nodes
-                    foreach (string objLocation in System.IO.Directory.GetFiles(catDirectory))
-                    {
-                        TreeNode newObject = new TreeNode(objLocation.Split('\\').Last().Split('.').First());
-                        newObject.Tag = objLocation;
-                        newCategory.Nodes.Add(newObject);
-                    }
-                }
-
-                // Expand Category List
-                categoryList.Expand();
+                refreshTreeView();
             }
 
+        }
+
+        private void refreshTreeView()
+        {
+            // clear list
+            this.objectBrowser.Nodes.Clear();
+
+            // Initial Node "Categories"
+            TreeNode categoryList = new TreeNode("Categories");
+            this.objectBrowser.Nodes.Add(categoryList);
+
+            foreach (string catDirectory in System.IO.Directory.GetDirectories(objectDirectory))
+            {
+                // Category Nodes
+                TreeNode newCategory = new TreeNode(catDirectory.Split('\\').Last());
+
+                // ignore subversion files
+                if (newCategory.Text != ".svn")
+                    categoryList.Nodes.Add(newCategory);
+
+                // Object Nodes
+                foreach (string objLocation in System.IO.Directory.GetFiles(catDirectory))
+                {
+                    TreeNode newObject = new TreeNode(objLocation.Split('\\').Last().Split('.').First());
+                    newObject.Tag = objLocation;
+                    newCategory.Nodes.Add(newObject);
+                }
+            }
+
+            // Expand Category List
+            categoryList.Expand();
         }
 
         private void sldVoltage_Scroll(object sender, EventArgs e)
@@ -52,7 +75,9 @@ namespace WindowsFormsApplication1
 
         private void txtVoltage_TextChanged(object sender, EventArgs e)
         {
-            int voltValue = Math.Min(System.Convert.ToInt32(txtVoltage.Text), 200);
+            int voltValue = 0;
+            try { voltValue = Math.Min(System.Convert.ToInt32(txtVoltage.Text), 200); }
+            catch (Exception exception) {}
             sldVoltage.Value = voltValue;
             txtVoltage.Text = voltValue.ToString();
         }
@@ -61,11 +86,113 @@ namespace WindowsFormsApplication1
         {
             if (objectBrowser.SelectedNode.Tag != null)
             {
+                currentCategory = objectBrowser.SelectedNode.Parent.Text;
+                currentObject = objectBrowser.SelectedNode.Text;
                 objectModifier.Visible = true;
+                loadCurrentObject();
             }
             else
             {
+                if (objectBrowser.SelectedNode.Parent.Text == "Categories")
+                    currentCategory = objectBrowser.SelectedNode.Text;
                 objectModifier.Visible = false;
+            }
+        }
+
+        private void imageChooser_Click(object sender, EventArgs e)
+        {
+            if (pictureSelector.ShowDialog() == DialogResult.OK) {
+                imageChooser.Image = new Bitmap(pictureSelector.FileName);
+            }
+        }
+
+        private void pictureSelector_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        // Serializes currently selected object to "Root directory" / "category" / "objectname" .bin
+        private void saveCurrentObject()
+        {
+            string filePath = objectDirectory + "/" + currentCategory + "/" + currentObject + ".bin";
+
+            SerializableItem SO = new SerializableItem();
+            SO.conductive = chkConductive.Checked;
+            SO.magnetic = chkMagnetic.Checked;
+            SO.ferrous = chkFerrous.Checked;
+            SO.charge = sldVoltage.Value;
+            SO.icon = imageChooser.Image;
+            SO.name = txtName.Text;
+
+            Stream stream = File.Open(filePath, FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(stream, SO);
+            stream.Close();
+        }
+
+        // Reads in currently selected object at "Root directory" / "category" / "objectname" .bin
+        // sets gui compontents with values from file
+        private void loadCurrentObject()
+        {
+            string filePath = objectDirectory + "/" + currentCategory + "/" + currentObject + ".bin";
+            Stream stream = File.Open(filePath, FileMode.Open);
+
+            BinaryFormatter bf = new BinaryFormatter();
+            SerializableItem SO = (SerializableItem)bf.Deserialize(stream);
+
+            chkConductive.Checked = SO.conductive;
+            chkMagnetic.Checked = SO.magnetic;
+            chkFerrous.Checked = SO.ferrous;
+            sldVoltage.Value = SO.charge;
+            imageChooser.Image = SO.icon;
+            txtName.Text = SO.name;
+
+            stream.Close();
+        }
+
+        private void saveObject_Click(object sender, EventArgs e)
+        {
+            saveCurrentObject();
+        }
+
+        // Creates a new object category directory
+        private void createNewCategory_Click(object sender, EventArgs e)
+        {
+            string catName = Microsoft.VisualBasic.Interaction.InputBox("Enter the category name", "New Category", "Default", 0, 0);
+            System.IO.Directory.CreateDirectory(System.IO.Path.Combine(objectDirectory, catName));
+
+            refreshTreeView();
+        }
+
+        private void createNewObject_Click(object sender, EventArgs e)
+        {
+            string objName = Microsoft.VisualBasic.Interaction.InputBox("Enter the object name", "New Object", "Default", 0, 0);
+            string filePath = objectDirectory + "/" + currentCategory + "/" + objName + ".bin";
+            SerializableItem SO = new SerializableItem();
+
+            SO.name = objName;
+            Stream stream = File.Open(filePath, FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(stream, SO);
+            stream.Close();
+
+            refreshTreeView();
+        }
+
+        [Serializable]
+        public class SerializableItem
+        {
+            public bool conductive, magnetic, ferrous;
+            public int charge;
+            public Image icon;
+            public string name;
+
+            public SerializableItem()
+            {
+                conductive = false;
+                magnetic = false;
+                ferrous = false;
+                charge = 0;
             }
         }
     }
