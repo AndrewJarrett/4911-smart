53cfd16d1d92bb26c56be7b7deff31d4cfdbec7a
efoley <efoley@030f3106-02ce-11df-8a57-81850bede9db>
2010-04-05 03:55:59 +0000

    * Added SerializedItem class * moved object directory to "bin/debug" * Added constructors to Toolbox and ToolboxCategory to deserialize items.
    Deserialization is not working so don't try it :)

---
+++ LabBench/SMART LabBench/language/ui/SerializableItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LabBench.language.ui
+{
+    [Serializable]
+    public class SerializableItem
+    {
+        public bool conductive, magnetic, ferrous;
+        public int charge;
+        public Image icon;
+        public string name;
+
+        public SerializableItem()
+        {
+            conductive = false;
+            magnetic = false;
+            ferrous = false;
+            charge = 0;
+        }
+    }
+}
