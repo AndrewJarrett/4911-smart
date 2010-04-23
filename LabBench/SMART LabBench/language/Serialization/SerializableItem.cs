9af703afca9ab36a4a88e42debded1339d2f2aa8
efoley <efoley@030f3106-02ce-11df-8a57-81850bede9db>
2010-04-23 18:06:07 +0000

    added lesson plan saving
    still not working, problem with serializing thumbnails
    
    to test, use green rectangle in lesson creator.

--- LabBench/SMART LabBench/language/Serialization/SerializableItem.cs
+++ LabBench/SMART LabBench/language/Serialization/SerializableItem.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace LabBench.language.ui
+namespace LabBench.language
 {
     [Serializable]
     public class SerializableItem
