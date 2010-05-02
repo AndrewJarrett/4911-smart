2fc844a206c2c0b164e3a154186a9fa15f585106
efoley <efoley@030f3106-02ce-11df-8a57-81850bede9db>
2010-05-02 02:13:39 +0000

    Lesson Chooser
    Initial commit for lesson chooser.
    
    added LessonChooser and LessonChoice classes,
    used to choose lessons from file or create new lessons
    working:
    -load lessons from files
    -scrolling through files 6 at a time
    
    to do:
    -open lessons after loading
    -create new lessons
    -preview on buttons
    -visual improvements (make things bigger)

---
+++ trunk/LabBench/SMART LabBench/language/ui/LessonChoice.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using libSMARTMultiTouch.Controls;
+using libSMARTMultiTouch.Input;
+using LabBench.language.circuit;
+
+namespace LabBench
+{
+    class LessonChoice : language.Icon
+    {
+        private Canvas parent;
+        private int locX, locY;
+
+        public LessonChoice(Canvas mainCanvas, int x, int y)
+            : base(new language.ImagePNG("ui/plus_sign.png"))
+        {
+            this.parent = mainCanvas;
+            this.locX = x; this.locY = y;
+
+            TransformGroup mTransformGroup = new TransformGroup();
+            mTransformGroup.Children.Add(new ScaleTransform(0.5, 0.5));
+            mTransformGroup.Children.Add(new TranslateTransform(x - 50, y - 75));
+
+            this.RenderTransform = mTransformGroup;
+
+            TouchInputManager.AddTouchContactDownHandler(this, new TouchContactEventHandler(Button_NewLesson));
+        }
+
+        public LessonChoice(Canvas mainCanvas, SerializedLesson lesson, int x, int y)
+            : base(new language.ImagePNG("ui/plus_sign.png"))
+        {
+            this.parent = mainCanvas;
+            this.locX = x; this.locY = y;
+
+            TransformGroup mTransformGroup = new TransformGroup();
+            mTransformGroup.Children.Add(new ScaleTransform(0.5, 0.5));
+            mTransformGroup.Children.Add(new TranslateTransform(x - 50, y - 75));
+
+            this.RenderTransform = mTransformGroup;
+
+            TouchInputManager.AddTouchContactDownHandler(this, new TouchContactEventHandler(Button_ChooseLesson));
+
+        }
+
+        private void Button_ChooseLesson(object sender, TouchContactEventArgs e)
+        {
+
+        }
+
+        private void Button_NewLesson(object sender, TouchContactEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ trunk/LabBench/SMART LabBench/language/ui/LessonChooser.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Windows.Media;
+using libSMARTMultiTouch.Input;
+
+namespace LabBench.language.ui
+{
+    class LessonChooser
+    {
+        List<Icon> visible;
+        List<SerializedLesson> lessonchoices;
+
+        BinaryFormatter bformatter;
+        Stream stream;
+
+        Canvas canvas;
+        int x, y;
+        int currentPage;
+
+        public LessonChooser(Canvas canvas, int x, int y)
+        {
+            this.canvas = canvas;
+            this.x = x; this.y = y;
+
+            bformatter = new BinaryFormatter();
+            lessonchoices = new List<SerializedLesson>();
+            visible = new List<Icon>();
+            string lessonDirectory = "lessons";
+
+            foreach (string file in System.IO.Directory.GetFiles(lessonDirectory))
+            {
+                if (!file.Split('\\').Contains(".svn"))
+                    lessonchoices.Add(deserializeItem(file));
+            }
+            this.currentPage = 0;
+            this.displayLessons(0);
+
+        }
+
+        public void displayLessons(int page)
+        {
+            this.clearLessons();
+            if (page > 0)
+                showLeftArrow();
+            showRightArrow();
+
+            for (int i = 0; i < 6; i++)
+            {
+                LessonChoice element;
+                int index = (6 * page) + i;
+                int posx = x + (i * 100) - (300 * (i / 3));
+                int posy = y + ((i / 3) * 100);
+                if (index < lessonchoices.Count)
+                    element = new LessonChoice(canvas, lessonchoices.ElementAt(index), posx, posy);
+                else
+                    element = new LessonChoice(canvas, posx, posy);
+
+                canvas.Children.Add(element);
+                visible.Add(element);
+            }
+        }
+
+        private void showRightArrow()
+        {
+            language.Icon right_arrow = new language.Icon(new language.ImagePNG("ui/right_arrow.png"));
+
+            TransformGroup transformGroup = new TransformGroup();
+            transformGroup.Children.Add(new ScaleTransform(0.5, 0.5));
+            transformGroup.Children.Add(new TranslateTransform(x + 270, y - 20));
+
+            right_arrow.RenderTransform = transformGroup;
+
+            TouchInputManager.AddTouchContactDownHandler(right_arrow, new TouchContactEventHandler(Button_NextPage));
+
+            canvas.Children.Add(right_arrow);
+            visible.Add(right_arrow);
+        }
+
+        private void showLeftArrow()
+        {
+            language.Icon left_arrow = new language.Icon(new language.ImagePNG("ui/left_arrow.png"));
+
+            TransformGroup transformGroup = new TransformGroup();
+            transformGroup.Children.Add(new ScaleTransform(0.5, 0.5));
+            transformGroup.Children.Add(new TranslateTransform(x - 100, y - 20));
+
+            left_arrow.RenderTransform = transformGroup;
+
+            TouchInputManager.AddTouchContactDownHandler(left_arrow, new TouchContactEventHandler(Button_PrevPage));
+
+            canvas.Children.Add(left_arrow);
+            visible.Add(left_arrow);
+        }
+
+        private void Button_NextPage(object sender, TouchContactEventArgs e)
+        {
+            currentPage = currentPage + 1;
+            this.displayLessons(currentPage);
+        }
+
+        private void Button_PrevPage(object sender, TouchContactEventArgs e)
+        {
+            currentPage = currentPage - 1;
+            this.displayLessons(currentPage);
+        }
+
+        public void clearLessons()
+        {
+            foreach (Icon lesson in visible)
+            {
+                canvas.Children.Remove(lesson);
+            }
+            visible = new List<Icon>();
+        }
+
+        private SerializedLesson deserializeItem(string objLocation)
+        {
+            stream = File.Open(objLocation, FileMode.Open);
+
+            SerializedLesson obj = (SerializedLesson)bformatter.Deserialize(stream);
+
+            stream.Close();
+            return obj;
+        }
+    }
+}
